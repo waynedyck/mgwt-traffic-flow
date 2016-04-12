2c38886334bbcee9774545dd9bf8ce098204288f
Wayne Dyck <wayne@imaginedreal.com>
2016-04-12 06:26:53 -0700

    Update app version string to 1.0.0

--- src/main/java/com/imaginedreal/mgwt/trafficflow/client/util/Consts.java
+++ src/main/java/com/imaginedreal/mgwt/trafficflow/client/util/Consts.java
@@ -30,7 +30,7 @@ public class Consts {
 	/**
 	 * Current version number and build of the app.
 	 */
-	public static final String APP_VERSION = "0.3.0";
+	public static final String APP_VERSION = "1.0.0";
 
     /**
      * Ad unit Id for banner
